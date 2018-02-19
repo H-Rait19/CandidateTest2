8b409509a18da1f477dad7765e7cb8c765b0192a
H-Rait19 <harmeet_s_r@hotmail.co.uk>
2018-02-19 07:37:35 +0000

    chgne

--- PairingTest.Unit.Tests/QuestionServiceWebApi/QuestionRepositoryTests.cs
+++ PairingTest.Unit.Tests/QuestionServiceWebApi/QuestionRepositoryTests.cs
@@ -19,5 +19,18 @@ namespace PairingTest.Unit.Tests.QuestionServiceWebApi
             Assert.That(questionnaire.QuestionsText[2], Is.EqualTo("What is the capital of Poland?"));
             Assert.That(questionnaire.QuestionsText[3], Is.EqualTo("What is the capital of Germany?"));
         }
+
+        [Test]
+        public void ShouldGetExpectedQuestionnaireAnswers()
+        {
+            var questionRepository = new QuestionRepository();
+
+            var questionnaire = questionRepository.GetAnswers();
+
+            Assert.That(questionnaire[0], Is.EqualTo("Havana"));
+            Assert.That(questionnaire[1], Is.EqualTo("Paris"));
+            Assert.That(questionnaire[2], Is.EqualTo("Warsaw"));
+            Assert.That(questionnaire[3], Is.EqualTo("Berlin"));
+        }
     }
 }
